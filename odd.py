718e17423a6367c6c314d62f86a923366be5250d
MeTooNeed <43276913+MeTooNeed@users.noreply.github.com>
2018-09-14 19:29:56 +0300

    Add files via upload

---
+++ odd.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from os import getcwd
+import random
+from time import sleep
+
+# Инверсия массива
+# Циклический сдвиг влево и вправо
+# Решето Эратосфена
+
+A = [i for i in 'ваваоргеrghlcnvbfhtyr_u5rofofkqafsderxvzcsgfdrepoimfkv' ]
+A.sort()
+B = 'shemaeshemaeshemaeshemae'
+f = ['a']
+for i in B:
+    f.append(i) if i in A and i not in f else None
